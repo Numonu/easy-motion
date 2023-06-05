90ea37390fef30ca40a8834343b8459073f23326
Numonu <juan.villegas.dev@gmail.com>
2023-06-05 14:01:03 -0500

    (feat):create default options for main model

---
+++ src/utilities/defaults.ts
@@ -0,0 +1,18 @@
+const defaultOptions:IOptions = {
+    aceleration : {
+        x : 10,
+        y : 10
+    },
+    maxSpeed : {
+        x : 120,
+        y: 120,
+    },
+    moveLimits : {
+        x : 500,
+        y : 500
+    }
+}
+
+export {
+    defaultOptions
+}
