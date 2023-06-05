a8c5da544e5d4c4c5682bc9caaef52d412d74640
Numonu <juan.villegas.dev@gmail.com>
2023-06-05 16:15:36 -0500

    (feat):add method for direction controller in motion model

--- src/models/motionElement.ts
+++ src/models/motionElement.ts
@@ -10,4 +10,25 @@ class MotionElement{
         this.options = options ? getDefaults(options , defaultOptions) : defaultOptions;
         this.parameters = defaultParameters;
     }
+    setDirection(_mousePos:IVector2){
+        const sensibility = 3;
+        //Axe-x
+        if (_mousePos.x >= this.parameters.lastPos.x + sensibility) {
+            if (this.parameters.direction.x == -1) this.parameters.allowSeed.x = true;
+            this.parameters.direction.x = 1;
+        }
+        else if (_mousePos.x <= this.parameters.lastPos.x - sensibility) {
+            if (this.parameters.direction.x == 1) this.parameters.allowSeed.x = true;
+            this.parameters.direction.x = -1;
+        }
+        //Axe-y
+        if (_mousePos.y >= this.parameters.lastPos.y + sensibility) {
+            if (this.parameters.direction.y == -1) this.parameters.allowSeed.y = true;
+            this.parameters.direction.y = 1;
+        }
+        else if (_mousePos.y <= this.parameters.lastPos.y - sensibility) {
+            if (this.parameters.direction.y == 1) this.parameters.allowSeed.y = true;
+            this.parameters.direction.y = -1;
+        }
+    }
 }
