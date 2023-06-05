aa0ad442f25453fb1e1d7b1fda01517ffe33c840
Numonu <juan.villegas.dev@gmail.com>
2023-06-05 16:40:42 -0500

    (update):create inline interface for special parameters

--- src/utilities/interfaces.ts
+++ src/utilities/interfaces.ts
@@ -1,20 +1,20 @@
-interface IVector2{
-    x : number,
-    y : number
+interface IVector2 {
+	x: number;
+	y: number;
 }
 
-interface IOptions{
-    aceleration:IVector2,
-    maxSpeed : IVector2,
-    moveLimits : IVector2
+interface IOptions {
+	aceleration: IVector2;
+	maxSpeed: IVector2;
+	moveLimits: IVector2;
 }
 
-interface IParameters{
-    localPos : IVector2,
-    lastPos: IVector2,
-    //
-    direction :IVector2,
-    //
-    seedPos : IVector2,
-    allowSeed : {x:boolean , y:boolean}
+interface IParameters {
+	localPos: IVector2;
+	lastPos: IVector2;
+	//
+	direction: {x : 1 | -1 , y : 1 | -1}
+	//
+	seedPos: IVector2;
+	allowSeed: { x: boolean; y: boolean };
 }
