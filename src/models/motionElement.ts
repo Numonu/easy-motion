ac87a6fbbeff73be34d1de10640beddb5caacbf1
Numonu <juan.villegas.dev@gmail.com>
2023-06-05 16:01:01 -0500

    (feat):motion model use default parameters for its own property

--- src/models/motionElement.ts
+++ src/models/motionElement.ts
@@ -1,11 +1,13 @@
 import { getDefaults } from "../lib/motionTools";
-import { defaultOptions } from "../utilities/defaults";
+import { defaultOptions, defaultParameters } from "../utilities/defaults";
 
 class MotionElement{
     target:HTMLElement
     options:IOptions
+    parameters:IParameters
     constructor(target:HTMLElement , options?:IOptions){
         this.target = target;
         this.options = options ? getDefaults(options , defaultOptions) : defaultOptions;
+        this.parameters = defaultParameters;
     }
 }
