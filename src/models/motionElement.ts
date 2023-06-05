227cb535b35d326c7d3f80fc9ebd9ad51d99c0d5
Numonu <juan.villegas.dev@gmail.com>
2023-06-05 12:55:25 -0500

    (feat)main model setup using recent interfaces

---
+++ src/models/motionElement.ts
@@ -0,0 +1,8 @@
+class MotionElement{
+    target:HTMLElement
+    options:IOptions
+    constructor(target:HTMLElement , options:IOptions){
+        this.target = target;
+        this.options = options;
+    }
+}
